e0abdec655b7befa37c42c44773c811d8b70882b
ticvictor2000 <ticvictor2000@gmail.com>
2020-06-14 19:31:54 +0200

    v0.39

--- view/users.php
+++ view/users.php
@@ -9,8 +9,8 @@
 
 <head>
   <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/logo.jpg">
+  <link rel="icon" type="image/png" href="../assets/img/logo.jpg">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>Gestión de usuarios</title>
   <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
